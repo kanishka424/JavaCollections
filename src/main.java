043c0c5adb3b998fb4ed8aed1e3c45b0ec2e613c
kanishka424 <kanishkamalpana24@gmail.com>
2021-02-03 10:51:10 +0530

    with ort

--- src/main.java
+++ src/main.java
@@ -45,5 +45,14 @@ public class main {
 
         }
 
+    public static void sortList(List<? extends Theatre.Seat> list) {
+        for (int i = 0; i < list.size() - 1; i++) {
+            for (int j = i + 1; j < list.size(); j++) {
+                if (list.get(i).compareTo(list.get(j)) > 0) {
+                    Collections.swap(list, i, j);
+                }
+            }
+        }
 
+    }
 }
